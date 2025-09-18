4f4cf01803f100247b90f2d3522eb2f5e52e2c06
osamamohamedr1 <osamarizk262@gmail.com>
2025-09-18 12:40:36 +0300

    add app icon

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:news_app_task/core/routing/routes.dart';
 import 'package:news_app_task/core/utils/extensions.dart';
 import 'package:news_app_task/features/home/presentation/manager/cubit/home_cubit.dart';
@@ -12,16 +13,17 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xff001F3F),
         scrolledUnderElevation: 0,
         elevation: 0,
         automaticallyImplyLeading: false,
-        title: const Text('Home View'),
+        title: SvgPicture.asset('assets/images/app_title.svg'),
         actions: [
           IconButton(
             onPressed: () {
               context.pushNamed(Routes.search);
             },
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.search, color: Colors.white),
           ),
         ],
       ),
